bada3a859084e157b1a435689bce60c7f0265dc6
Мария Акимкина <wapovalova@gmail.com>
2024-07-11 11:50:00 +0300

    add src/locales/en.js, src/locales/ru.js, src/locales/index.js

---
+++ src/locales/en.js
@@ -0,0 +1,17 @@
+export default {
+    translation: {
+      feedbacks: {
+        feedbackSuccess: 'RSS successfully loaded',
+        feedbackWrongURL: 'Link must be valid URL',
+        feedbackRepeat: 'RSS already exists',
+        feedbackNoRSS: "RSS doesn't contain valid RSS",
+      },
+      posts: 'Posts',
+      feeds: 'Feeds',
+      buttons: {
+        viewBtn: 'View',
+        readFullBtn: 'Read the full version',
+        closeBtn: 'Close',
+      },
+    },
+  };
---
+++ src/locales/ru.js
@@ -0,0 +1,17 @@
+export default {
+    translation: {
+      feedbacks: {
+        feedbackSuccess: 'RSS успешно загружен',
+        feedbackWrongURL: 'Ссылка должна быть валидным URL',
+        feedbackRepeat: 'RSS уже существует',
+        feedbackNoRSS: "RSS не содержит валидного RSS",
+      },
+      posts: 'Посты',
+      feeds: 'Фиды',
+      buttons: {
+        viewBtn: 'Посмотр',
+        readFullBtn: 'Читать полностью',
+        closeBtn: 'Закрыть',
+      },
+    },
+  };
