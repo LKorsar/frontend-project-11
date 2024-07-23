e5d9cfd38efb36a00dfdf1e465fcc3734d3fc204
Мария Акимкина <wapovalova@gmail.com>
2024-07-23 16:30:05 +0300

    update index.html, src/index.js and src/view.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,46 @@
 import  './styles.scss';
 import  'bootstrap';
 import * as yup from 'yup';
-import onChange from 'on-change';
 import _ from 'lodash';
-import i18n from 'i18next';
+import i18next, { init } from 'i18next';
 import resources from './locales/index.js';
-
-const render = (watchedState, elements, i18nInstance) => {
-  if (watchedState.searchForm.valid !== true || watchedState.searchForm.valid !== null) {
-    elements.input.classList.add('text-danger');
-    elements.feedback.textContent = watchedState.searchForm.errors;
-  } else {
-    elements.input.classList.remove('text-danger');
-    elements.input.classList.add('text-success');
-    elements.feedback.textContent = i18nInstance.t('feedbacks.feedbackSuccess');
-  }
-  elements.form.reset();
-  elements.input.focus();
-};
+import view from './view.js';
 
 const app = () => {
-  const i18nInstance = i18n.createInstance();
-  i18nInstance.init({
-    lng: 'ru',
-    debug: false,
-    resources,
-  });
-
-  const state = {
-    searchForm: {
-      valid: null,
-      errors: {},
-    },
-    inputUrl: '',
-    posts: [],
-    feeds: [],
-  };
-  
   const elements = {
     form: document.querySelector('.rss-form'),
     input: form.querySelector('#url-input'),
     postsList: document.querySelector('.posts'),
     feedback: document.querySelector('.feedback'),
   };
-  
-  const watchedState = onChange(state, render);
 
-  const loadedFeeds = state.feeds;
+  const state = {
+    processApp: {
+      processState: 'filling',
+      processError: null,
+    },
+    rssForm: {
+      valid: null,
+      errors: [],
+      inputUrl: '',
+    },
+    posts: [],
+    feeds: [],
+  };
+   
+  const { watchedState } = view(elements, i18n, state);
+
+  const defaultLang = 'ru';
+  const i18n = i18next.createInstance();
+  i18n.init({
+    lng: defaultLang,
+    debug: false,
+    resources,
+  })
+    .then(() => console.log('i18next instance initiated successfully'))
+    .catch(() => console.log('i18next instance caused an error'));
+
+  const loadedFeeds = watchedState.feeds;
 
   const schema = yup.object({
     inputUrl: yup.string()
@@ -59,26 +53,29 @@ const app = () => {
     schema.validate({ inputUrl })
       .then(() => console.log('validation acomplished successfully'))
       .catch((err) => console.log(err.errors))
-};
+  };
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const data = formData.get('url');
-    state.inputUrl = data;
-    const errors = validateURL({ inputUrl: data });
-    state.searchForm.errors = errors;
-    state.searchForm.valid = _.isEmpty(errors);
+    watchedState.rssForm.inputUrl = data;
+    const validationErrors = schema.validate({ inputUrl: watchedState.rssForm.inputUrl })
+      .then(() => console.log('validation acomplished successfully'))
+      .catch((error) => {
+        return error.message
+      });
+    watchedState.rssForm.errors = validationErrors;
+    watchedState.rssForm.valid = _.isEmpty(errors);
     if (_.isEmpty(errors)) {
       // sending request to server
       //const response
-      //state.feeds.push(data);
-      //state.posts.push(response.items);
+    }
+    else {
+
     }
   });
-  render(watchedState, elements, i18nInstance);
+
 };
 
 export default app;
-
-//app();
---
+++ src/view.js
@@ -0,0 +1,39 @@
+import onChange from 'on-change';
+
+const renderSuccessFeedBack = (elements, i18n) => {
+  elements.input.classList.remove('text-danger');
+  elements.input.classList.add('text-success');
+  elements.feedback.textContent = i18n.t('feedbacks.feedbackSuccess');
+  elements.form.reset();
+  elements.input.focus();
+};
+
+const renderError = (elements, state) => {
+  elements.input.classList.remove('text-success');  
+  elements.input.classList.add('text-danger');
+  elements.feedback.textContent = state.rssForm.errors;
+  elements.form.reset();
+  elements.input.focus();
+};
+
+export default (elements, i18n, state) => {
+  const watchedState = onChange(state, (path, value) => {
+    switch (path) {
+      case 'processApp.processState':
+        
+        break;
+      case 'processApp.processError':
+
+        break;
+      case 'rssForm.valid':
+        renderSuccessFeedBack(elements, i18n);
+        break;
+      case 'rssForm.errors':
+        renderError(elements, state);
+        break;
+      default:
+        break;      
+    }
+  });
+return watchedState;
+};
