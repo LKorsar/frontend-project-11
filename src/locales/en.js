4bee407ed090fac14fafbba864e05c39bdd73990
Мария Акимкина <wapovalova@gmail.com>
2024-09-09 15:32:07 +0300

    update src/locales/index.js, src/locales/ru.js and src/locales/en.js

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,18 +1,19 @@
 export default {
-    translation: {
-      feedbacks: {
-        feedbackSuccess: 'RSS successfully loaded',
-        feedbackWrongURL: 'Link must be valid URL',
-        feedbackRepeat: 'RSS already exists',
-        feedbackNoRSS: "RSS doesn't contain valid RSS",
-      },
-      posts: 'Posts',
-      feeds: 'Feeds',
-      buttons: {
-        viewBtn: 'View',
-      },
-      errors: {
-        network: 'Network error',
-      },
+  translation: {
+    feedbacks: {
+      feedbackSuccess: 'RSS successfully loaded',
+      feedbackWrongURL: 'Link must be valid URL',
+      feedbackRepeat: 'RSS already exists',
+      feedbackNoRSS: "RSS doesn't contain valid RSS",
+      feedbackEmpty: 'Must not be empty',
     },
-  };
+    posts: 'Posts',
+    feeds: 'Feeds',
+    buttons: {
+      viewBtn: 'View',
+    },
+    errors: {
+      network: 'Network error',
+    },
+  },
+};
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,18 +1,19 @@
 export default {
-    translation: {
-      feedbacks: {
-        feedbackSuccess: 'RSS успешно загружен',
-        feedbackWrongURL: 'Ссылка должна быть валидным URL',
-        feedbackRepeat: 'RSS уже существует',
-        feedbackNoRSS: "Ресурс не содержит валидный RSS",
-      },
-      posts: 'Посты',
-      feeds: 'Фиды',
-      buttons: {
-        viewBtn: 'Посмотр',
-      },
-      errors: {
-        network: 'Ошибка сети',
-      },
+  translation: {
+    feedbacks: {
+      feedbackSuccess: 'RSS успешно загружен',
+      feedbackWrongURL: 'Ссылка должна быть валидным URL',
+      feedbackRepeat: 'RSS уже существует',
+      feedbackNoRSS: 'Ресурс не содержит валидный RSS',
+      feedbackEmpty: 'Не должно быть пустым',
     },
-  };
+    posts: 'Посты',
+    feeds: 'Фиды',
+    buttons: {
+      viewBtn: 'Посмотр',
+    },
+    errors: {
+      network: 'Ошибка сети',
+    },
+  },
+};
